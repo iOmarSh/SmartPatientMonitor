8d35f6b56cf245c5ebde241bfd5a65c25319d301
Omar Shawky <omargenius2013@gmail.com>
2026-05-15 01:21:11 +0300

    Ya shaweesh
    hshshs

--- src/sensors/ds18b20_sensor.cpp
+++ src/sensors/ds18b20_sensor.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#include "pins.h"
 #include "sensors/ds18b20_sensor.h"
 
-#ifndef DS18_PIN
-#define DS18_PIN 4
-#endif
+namespace
+{
+    OneWire oneWire(PIN_DS18B20);
+    DallasTemperature ds18b20(&oneWire);
+    bool sensorReady = false;
+}
 
 void initTemperatureSensor()
 {
-    // Placeholder initialization: DS18B20 typically needs OneWire; add later
-    pinMode(DS18_PIN, INPUT_PULLUP);
+    pinMode(PIN_DS18B20, INPUT_PULLUP);
+    ds18b20.begin();
+    sensorReady = true;
 }
 
 float readTemperature()
 {
-    // Return a dummy value until a OneWire driver is added
-    return 25.0f;
+    if (!sensorReady)
+    {
+        return NAN;
+    }
+
+    ds18b20.requestTemperatures();
+    const float temperatureC = ds18b20.getTempCByIndex(0);
+
+    if (temperatureC == DEVICE_DISCONNECTED_C)
+    {
+        return NAN;
+    }
+
+    return temperatureC;
 }
--- src/tasks/task_sensor.cpp
+++ src/tasks/task_sensor.cpp
@@ -3,6 +3,8 @@
 #include "freertos/task.h"
 #include "tasks/task_sensor.h"
 #include "sensors/ldr_sensor.h"
+#include "sensors/ds18b20_sensor.h"
+#include "sensors/mpu6050_sensor.h"
 #include "sensors/button_input.h"
 
 void TaskSensor(void *pvParameters)
@@ -10,10 +12,35 @@ void TaskSensor(void *pvParameters)
     while (1)
     {
         int ldrValue = readLDR();
+        float temperatureC = readTemperature();
+        float accelerationMagnitude = readAccelerationMagnitude();
         bool buttonPressed = isEmergencyPressed();
-        Serial.print("[LDR] ADC value: ");
-        Serial.println(ldrValue);
-        Serial.print("[Button GPIO18] State: ");
+
+        Serial.print("[Sensors] LDR=");
+        Serial.print(ldrValue);
+        Serial.print(" | DS18B20=");
+        if (isnan(temperatureC))
+        {
+            Serial.print("N/A");
+        }
+        else
+        {
+            Serial.print(temperatureC, 2);
+            Serial.print(" C");
+        }
+
+        Serial.print(" | MPU6050 |acc|=");
+        if (isnan(accelerationMagnitude))
+        {
+            Serial.print("N/A");
+        }
+        else
+        {
+            Serial.print(accelerationMagnitude, 2);
+            Serial.print(" m/s^2");
+        }
+
+        Serial.print(" | Button=");
         Serial.println(buttonPressed ? "PRESSED" : "RELEASED");
 
         vTaskDelay(pdMS_TO_TICKS(1000));
